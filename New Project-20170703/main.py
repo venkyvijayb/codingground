44087409b0b1afac1f2fe0bebc3bbe104c3823d9
venkyvijayb <venkateshbylla01@gmail.com>
2017-07-03 23:17:40 +0530

    Created by Coding Ground Client

---
+++ New Project-20170703/main.py
@@ -0,0 +1,31 @@
+# Hello World program in Python
+    
+print "hello "+\
+     "world"; print "keep going"
+integer = 101;
+print integer+10;
+name = age = 'venkat'
+print name+" is a good boy " + age
+print name+str(integer)
+print eval(name)
+a,b,c=10,20,30
+print a+1,b+2,c+3
+print name
+print name[0]
+print name[2:4]
+print name[1:]
+print name*3
+name = ['b','c']
+print name[0],name[1]
+
+#dictionary
+print "Dictionary"
+dic = {}
+dic['name']='venkat'
+dic['age']=20
+print dic
+print dic['name']
+dic['name']+='esh bylla'
+print dic['name']
+print dic.keys()
+print dic.values()
